62b8e23b84b6db4bd4928b0fa6720792aa72bb5e
Martin Richard <martius@martiusweb.net>
2010-11-18 22:19:51 +0100

    Structure decorator implemented, must implement Collection now

---
+++ lib/YaPhpDoc/Generator/Decorator/Collection.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * YaPhpDoc
+ * @author Martin Richard
+ * @license New BSD License
+ */
+
+/**
+ * This class is a collection decorator, and decorates tokens retrieved through
+ * a collection.
+ * 
+ * @author Martin Richard
+ */
+class YaPhpDoc_Generator_Decorator_Collection
+{
+	// TODO
+}
