ffa882877ba0585b34166569a7595c22d7774e59
Martin Richard <martius@martiusweb.net>
2010-12-20 00:29:01 +0100

    Preparing Tokenizer tests

---
+++ tests/lib/YaPhpDoc/Tokenizer.test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * YaPhpDoc
+ * @author Martin Richard
+ * @license New BSD License
+ */
+
+require_once __DIR__.'/../../util.php';
+require_once 'YaPhpDoc/Tokenizer.php';
+
+class TokenizerTest extends PHPUnit_Framework_TestCase
+{
+}
